d1cd1ec82f1f6a1e5fba86d2c18d0ef68aabb0ab
Ken MacKeen <wellnessgardener@gmail.com>
2021-02-14 16:48:29 -0500

    Updates

---
+++ precache-manifest.ac274be263df33d774be5782510bc90f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1bf44b3d104bd6d218e",
+    "url": "/defi_tutorial/static/css/main.39c1894d.chunk.css"
+  },
+  {
+    "revision": "b1bf44b3d104bd6d218e",
+    "url": "/defi_tutorial/static/js/main.b1bf44b3.chunk.js"
+  },
+  {
+    "revision": "7aae1a4cd1097c197d56",
+    "url": "/defi_tutorial/static/css/1.f475ea14.chunk.css"
+  },
+  {
+    "revision": "7aae1a4cd1097c197d56",
+    "url": "/defi_tutorial/static/js/1.7aae1a4c.chunk.js"
+  },
+  {
+    "revision": "0e03bbeb462c41702cd7",
+    "url": "/defi_tutorial/static/js/runtime~main.0e03bbeb.js"
+  },
+  {
+    "revision": "26eb8fcf057666d7db8e1dac74bcac55",
+    "url": "/defi_tutorial/index.html"
+  }
+];
